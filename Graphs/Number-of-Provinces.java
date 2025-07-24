f4b9e5f5a9a38fde8cadf7260bf50302fcde56fd
raiiden15 <126965753+raiiden15@users.noreply.github.com>
2025-07-24 13:27:49 +0530

    [547] [Time Beats: 17.57%] [Memory Beats: 80.82%] - LeetPush

--- Graphs/Number-of-Provinces.java
+++ Graphs/Number-of-Provinces.java
@@ -1,4 +1,17 @@
 class Solution {
+    public void BFS(boolean[] visited, int u, List<List<Integer>> adj, Queue<Integer> q) {
+        q.add(u);
+        visited[u] = true;
+        while(!q.isEmpty()) {
+            int curr = q.poll();
+            for (int v : adj.get(curr)) {
+                if (!visited[v]) {
+                    q.add(v);
+                    visited[v] = true;
+                }
+            }
+        }
+    }
     public void DFS(boolean visited[], int u, List<List<Integer>> adj) {
         if (visited[u]) return;
         visited[u] = true;
@@ -25,9 +38,12 @@ class Solution {
 
         int count = 0;
         boolean[] visited = new boolean[n];
+        Queue<Integer> q = new LinkedList<>();
+        
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
-                DFS(visited, i, adj);
+                // DFS(visited, i, adj);
+                BFS(visited, i, adj, q);
                 count++;
             }
         }
